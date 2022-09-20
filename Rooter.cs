37da70c1852ad7fa5bd495869da097dd523bc784
dega <brandondega@gmail.com>
2022-09-19 20:32:13 -0500

    Add project files.

---
+++ Rooter.cs
@@ -0,0 +1,16 @@
+﻿namespace MyMath
+{
+    public class Class1
+    {
+
+    }
+
+    public class Rooter
+    {
+        public double SquareRoot(double input)
+        {
+            double absoluteValue = Math.Abs(input);
+            return Math.Sqrt(absoluteValue);
+        }
+    }
+}
